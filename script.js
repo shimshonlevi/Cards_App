4c87ac3651573244c809755a668d2c27c798df1b
Shimshon <shimshonlevi384@gmail.com>
2024-09-11 20:41:41 +0300

    add

--- script.js
+++ script.js
@@ -18,21 +18,43 @@ const users = [
 
 ];
 
+function checkLoginStatus() {
+    const isLoggedIn = localStorage.getItem("isLoggedIn");
+    
+    // אם המשתמש מחובר, מעבר לדף הבית
+    if (isLoggedIn === "true") {
+        document.querySelector(".login-form").classList.add("display-not-active");
+        document.querySelector(".cards-page").classList.add("display-active");
+    }
+}
+
+checkLoginStatus();
+
 document.querySelector(".login-form > form").addEventListener("submit", (e) => {
     e.preventDefault();
     const userInputs = e.target;
-//     console.log(userEmailInput);
-// for (let index = 0; index < userEmailInput.length; index++) {
-//     console.log(userEmailInput[index].value);
-     
-    
-// }
-    if(users.find(user=>user.email === userInputs["email"].value && user.password === userInputs["password"].value)){
 
-        document.querySelector(".login-form").classList.toggle("display-not-active");
-        document.querySelector(".cards-page").classList.toggle("display-active");
-    }else{
+    // בדיקת האם המשתמש קיים ברשימת המשתמשים
+    const foundUser = users.find(user => 
+        user.email === userInputs["email"].value && user.password === userInputs["password"].value
+    );
+
+    if (foundUser) {
+        // אם התחבר בהצלחה, נשמור את המצב ב-Local Storage
+        localStorage.setItem("isLoggedIn", "true");
+
+        // מעבר לדף הבית
+        document.querySelector(".login-form").classList.add("display-not-active");
+        document.querySelector(".cards-page").classList.add("display-active");
+    } else {
         console.log("modal - big nono");
-        
     }
+});
+document.querySelector("header > #btn_logout").addEventListener("click", () => {
+    // ניקוי המידע מה-Local Storage
+    localStorage.removeItem("isLoggedIn");
+
+    // החזרה לעמוד ההתחברות
+    document.querySelector(".login-form").classList.remove("display-not-active");
+    document.querySelector(".cards-page").classList.remove("display-active");
 });
